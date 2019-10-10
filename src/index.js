0c3077a8a995333cc00c0c16368a98839c6fd05f
Deeamon <marinasugutova@gmail.com>
2019-10-10 18:54:49 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let countTwo = 0;
+  let countFive = 0;
+  let exp = expression.split('*');
+   
+  for (let i = 0; i < exp.length; i++) {
+    let flag;
+
+    (exp[i].includes('!!')) ? flag = 2 : flag = 1;
+    (exp[i].includes('!!')) ? exp[i] = exp[i].replace('!!', '') : exp[i] = exp[i].replace('!', '');
+
+
+    while (exp[i] >= 1 ) {
+      let temp = exp[i];
+      while (temp  % 5 == 0) {
+        countFive++;
+        temp /= 5;
+      }
+
+      temp  = exp[i];
+      while (temp  % 2 == 0) {
+        countTwo++;
+        temp /= 2;
+      }
+
+      exp[i] = exp[i] - flag;
+    }
+  }
+
+  return (countTwo < countFive) ? countTwo : countFive;
 }
